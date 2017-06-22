6cc71162e5984f61b9b3f0a3b80708de507ce515
Christoph Schmeisser <christoph@schmeisserweb.at>
2017-06-22 14:27:23 +0200

    immo suche

--- profiles/willhaben_immo.py
+++ profiles/willhaben_immo.py
@@ -24,9 +24,14 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+import re
+import urllib.parse
+import itertools
+
 from bs4 import BeautifulSoup
 from . import base
-import re
+from datetime import datetime
+from enum import Enum
 
 class WillhabenImmoProfile(base.ProfileBase):
     
@@ -40,38 +45,78 @@ class WillhabenImmoProfile(base.ProfileBase):
     def tags(self):
         return self._tags.keys()
 
+    @property
+    def encoding(self):
+        return "ISO-8859-1"
+
+    @property
+    def key_tag(self):
+        return "id"
+
+    @property
+    def datetime_tag(self):
+        return "datetime"
+
+
+    def first_page(self, url):
+        url_components = list(urllib.parse.urlparse(url))
+        query = dict(urllib.parse.parse_qs(url_components[4]))
+        query["page"] = 1
+        url_components[4] = urllib.parse.urlencode(query, doseq=True)
+        return urllib.parse.urlunparse(url_components)
+
+    def next_page(self, url):
+        url_components = list(urllib.parse.urlparse(url))
+        query = dict(urllib.parse.parse_qs(url_components[4]))
+        if "p" in query:
+            query["page"] = int(query["page"]) + 1
+        else:
+            query["p"] = 1
+        url_components[4] = urllib.parse.urlencode(query, doseq=True)
+        return urllib.parse.urlunparse(url_components)
+
     def parse(self, html):
         soup = BeautifulSoup(html)
-        allads = soup.find(name="ul", attrs={"id":"resultlist"})
-        ads = allads.findAll("li", attrs={"class":"even clearfix"})
-        ads.extend(allads.findAll("li", attrs={"class":"odd clearfix"}))
+        allads = soup.find(name="div", attrs={"id":"resultlist"})
+        ads = allads.findAll("article", attrs={"class":"search-result-entry"})
+        #print(ads)
+        #ads.extend(allads.findAll("li", attrs={"class":"odd clearfix"}))
         return list(map(self._ad_soup_to_dict, ads))
 
     def _ad_soup_to_dict(self, soup):
         tags = self._tags.copy()
-        tags["id"] = int(soup.h2.attrs['id'])
-        tags["url"] = self.base_url + soup.h2.a.attrs['href']        
-        tags["title"] = soup.h2.a.text
-        
-        size_text = soup.find('p', attrs={'class':'size'}).text
-        tags["size"] = int(re.findall("[0-9]+", size_text)[0])
-        
-        price_text = soup.find('p', attrs={'class':'price'}).text
-        price_text = "".join(re.findall("[0-9]+", price_text))
-        if len(price_text) > 0:
-            tags["price"] = int(price_text)
-        
-        description_text = soup.find('p', attrs={'class':'description'}).text
-        lines = re.findall("^[\r\n\s]*([^\r^\n]+)[\r\n\s]*", description_text)
-        if len(lines) > 0:
-            tags["description"] = lines[0]
+        print(
+            soup
+
+        )
+        tags["id"] = int(
             
-        location_text = soup.find('p', attrs={'class':'location'}).text
-        lines = re.findall("^[\r\n\s]*[0-9]+[\r\n\s]*([^\r^\n]+)[\r\n\s]*", location_text)
-        if len(lines) > 0:
-            tags["location"] = lines[0]
+            soup.find(name="div", attrs={"class":"header"}).find(name="a").attrs["data-ad-link"]
             
-        zip_text = re.findall("[0-9]+", location_text)[0]
-        tags["zipcode"] = int(zip_text)
+            )
+        print(tags["id"])
+        # tags["url"] = self.base_url + soup.h2.a.attrs['href']        
+        # tags["title"] = soup.h2.a.text
+        
+        # size_text = soup.find('p', attrs={'class':'size'}).text
+        # tags["size"] = int(re.findall("[0-9]+", size_text)[0])
+        
+        # price_text = soup.find('p', attrs={'class':'price'}).text
+        # price_text = "".join(re.findall("[0-9]+", price_text))
+        # if len(price_text) > 0:
+        #     tags["price"] = int(price_text)
+        
+        # description_text = soup.find('p', attrs={'class':'description'}).text
+        # lines = re.findall("^[\r\n\s]*([^\r^\n]+)[\r\n\s]*", description_text)
+        # if len(lines) > 0:
+        #     tags["description"] = lines[0]
+            
+        # location_text = soup.find('p', attrs={'class':'location'}).text
+        # lines = re.findall("^[\r\n\s]*[0-9]+[\r\n\s]*([^\r^\n]+)[\r\n\s]*", location_text)
+        # if len(lines) > 0:
+        #     tags["location"] = lines[0]
+            
+        # zip_text = re.findall("[0-9]+", location_text)[0]
+        # tags["zipcode"] = int(zip_text)
         
         return tags
